b9952f072132e09ae9798d8abeb0581ffa853211
Guido Di Salvo <guido.disalvoo@gmail.com>
2019-05-29 19:04:47 -0300

    Ajustes

--- custom.js
+++ custom.js
@@ -4,15 +4,20 @@ var map;
 
 function initMap() {
     // The location of Buenos Aires
-    var myLatlng = {lat: -34.6037389, lng: -58.3837591};
+    var myLatlng = {
+        lat: -34.6037389,
+        lng: -58.3837591
+    };
     // The map, centered at Buenos Aires
     map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 16, center: myLatlng});
+        document.getElementById('map'), {
+            zoom: 16,
+            center: myLatlng
+        });
     // The marker, positioned at Buenos Aires
-  }
+}
 
 function geolocate() {
-    alert('Pase a la función...');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             alert('Entree...');
@@ -37,5 +42,25 @@ function addMark(location) {
         position: location,
         map: map
     });
-            
+
 }
+
+
+//function that gets the location and returns it
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geo Location not supported by browser");
+    }
+}
+//function that retrieves the position
+function showPosition(position) {
+    var location = {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+    }
+    alert(location)
+}
+//request for location
+//   getLocation();
